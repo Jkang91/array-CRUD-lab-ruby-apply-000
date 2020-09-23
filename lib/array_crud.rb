3977f5f68644be59eabc81ecadf1c3947751214e
June Young Kang <jkang34@gmail.com>
2020-09-23 22:47:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  ["wow", "I", "am", "really", "learning"]
   ["wow", "I", "am", "really", "learning"][0]
 end
 
 def retrieve_last_element_from_array(array)
-  ["wow", "I", "am", "really", "learning"]
   ["wow", "I", "am", "really", "learning"][-1]
 end
