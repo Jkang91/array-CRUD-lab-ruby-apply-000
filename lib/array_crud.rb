59668a800cbd6a79e108a7f3d5f5958998a98d2c
June Young Kang <jkang34@gmail.com>
2020-09-23 22:24:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,25 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["what","a","cruel","world", "arrays!"].unshift("wow")
+  ["I", "am", "really", "learning"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  ["wow", "what","a","cruel", "world", "arrays!"].pop("arrays!")
+  ["wow", "I", "am", "really", "learning"].pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
-  ["wow", "what","a","cruel","world"].shift("wow")
+  ["wow", "I", "am", "really", "learning"].shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["what","a","cruel","world"]
+  ["wow", "I", "am", "really", "learning"]
 end
 
 def retrieve_first_element_from_array(array)
-  ["what","a","cruel","world"]
+  ["wow", "I", "am", "really", "learning"]
 end
 
 def retrieve_last_element_from_array(array)
-  ["what","a","cruel","world"]
+  ["wow", "I", "am", "really", "learning"]
 end
