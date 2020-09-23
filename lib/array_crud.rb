561964ea81cbca0683f548e645d4b54153f20eff
June Young Kang <jkang34@gmail.com>
2020-09-23 22:17:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["what","a","cruel","world"].unshift("wow")
+  ["what","a","cruel","world", "arrays!"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  ["what","a","cruel","arrays!"].pop("arrays!")
+  ["wow", "what","a","cruel", "world", "arrays!"].pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
