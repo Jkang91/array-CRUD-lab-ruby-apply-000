36c46f6ef3aa981b45e0a77a0c4f4522d6acf3a0
June Young Kang <jkang34@gmail.com>
2020-09-23 22:14:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["what","a","cruel","world"] << "array"
+  ["what","a","cruel","world"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
