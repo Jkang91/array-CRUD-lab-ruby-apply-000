3db479def20b6c7272b9060b921e3a1cc81d7e45
June Young Kang <jkang34@gmail.com>
2020-09-23 22:14:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["what","a","cruel","world"]
+  ["what","a","cruel","world"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
