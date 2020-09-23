762c303331cb69ede15e53541588907339d8a3e6
June Young Kang <jkang34@gmail.com>
2020-09-23 22:02:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  x = [1, 2, 3, 4]
   x << 5
 end
 
 def add_element_to_start_of_array(array, element)
+  x = [1, 2, 3, 4]
   x.unshift(0)
 end
 
